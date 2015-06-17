a39b152b5b788815ee008179c319a8909b5d0281
boertel <benjamin.oertel@gmail.com>
2015-06-16 17:04:33 -0700

    handle switching years with year(xxxx) in the console

--- reading/books.js
+++ reading/books.js
@@ -1,19 +1,12 @@
+d3.selection.prototype.pluralize = function () {
+    console.log(this);
+};
+
 var formats = {
     date: d3.time.format("%Y-%m-%d").parse
 };
 
-function showData(d) {
-    return function () {
-        var node = d3.select(this),
-            attribute = node.attr('data-attribute'),
-            format = node.attr("data-format") || ".1f";
-
-        node.text(d3.format(format)(d[attribute]));
-    }
-}
-
-d3.json("./data/books-2014.json", function (books) {
-    window.books = books;
+function preformat(books) {
     books.forEach(function (d) {
         d.start_date = formats.date(d.start_date);
         d.end_date = formats.date(d.end_date);
@@ -25,11 +18,14 @@ d3.json("./data/books-2014.json", function (books) {
     books.sort(function (a, b) {
         return (a.start_date < b.start_date) ? -1 : 1;
     });
+    return books;
+}
 
+function update (year, books) {
     var extrems = {
         dates: {
-            min: new Date("2014-01-01"),
-            max: new Date("2014-12-31")
+            min: new Date(year + '-01-01'),
+            max: new Date(year + '-12-31')
         },
         pages: {
             min: 0,
@@ -37,210 +33,126 @@ d3.json("./data/books-2014.json", function (books) {
         }
     };
 
-
-    var x = d3.time.scale()
-			.domain([extrems.dates.min, extrems.dates.max])
-            .range([0, width - padding.left - padding.right]);
-        y = d3.scale.linear()
-            .domain([extrems.pages.min, extrems.pages.max])
-            .rangeRound([height, 0]);
-
+    x.domain([extrems.dates.min, extrems.dates.max]);
+    y.domain([extrems.pages.min, extrems.pages.max]);
 
     /* Pages numbers and lines */
-    var yLines = svg.append("g")
-        .attr("transform", "translate(0, " + padding.top + ")")
-        .attr("class", "lines y")
-        .selectAll(".lines")
-        .data(y.ticks(3))
-      .enter()
-        .append("g")
-        .attr("transform", function (d) { return "translate(" + 0 + ", " + y(d) + ")"})
-    yLines
-        .append("line")
-        .attr("class", "dot")
-        .attr("y1", 0)
-        .attr("y2", 0)
-        .attr("x1", 0)
-        .attr("x2", width - padding.right + padding.left/2)
-    yLines
-        .append("text")
-        .attr("x", width - padding.right + padding.left)
-        .attr("y", 6)
-        .attr("text-anchor", "start")
-        .text(function (d, i) { return i == 0 ? "pages" : d; })
+    // Months abbreviation and lines
+    var xDots = xLines.selectAll(".line").data(x.ticks(12), function (d) { return d.getMonth(); })
 
+    xDots.exit()
+        .remove()
 
-    /* Bars */
-    svg.append("g")
-        .attr("transform", "translate(" + padding.left + ", " + (padding.top + 1) + ")")
-        .attr("class", "bars")
-        .selectAll(".bars")
-        .data(window.books)
-      .enter()
-        .append("g")
-        .attr("transform", function (d) { return "translate(" + x(d.start_date) + ", 0)"; })
-        .attr("class", function (d) { return "month-" + (d.start_date.getMonth() + 1); })
-        .append("rect")
-        .attr("class", "book")
-        .attr("y", function (d) { return y(d.pages); })
-        .attr("width", function (d) { return x(d.end_date) - x(d.start_date); })
-        .attr("height", function (d) { return height - y(d.pages); })
-
-    /* Months abbreviation and lines */
-    var xLines = svg.append("g")
-        .attr("transform", "translate(" + padding.left + ", " + (height + padding.top) + ")")
-        .attr("class", "lines x")
-        .selectAll(".lines")
-        .data(x.ticks(12))
-      .enter()
+    xDots.enter()
         .append("g")
         .attr("transform", function (d) { return "translate(" + x(d) + ", 0)"; })
+        .attr("class", "line")
 
-    xLines
+    xDots
         .append("line")
         .attr("class", function (d, i) { return i == 0 ? " hidden" : ""; })
         .attr("x1", 0)
         .attr("x2", 0)
         .attr("y1", 0)
         .attr("y2", 6)
-
-    xLines
-        .append("text")
+    xDots.append("text")
         .attr("x", 6)
         .attr("y", 30)
-        .text(function (d) { return d3.time.format("%b")(d); })
+        .text(function (d) {
+            return d3.time.format("%b")(d);
+        })
+    var yDots = yLines.selectAll('.line').data(y.ticks(3), function (d, i) { return d; })
 
+    yDots.exit()
+        .remove()
 
-    /* HTML representation */
-    var data = {
+    yDots.enter()
+        .append("g")
+        .attr("transform", function (d) { return "translate(" + 0 + ", " + y(d) + ")"})
+        .attr("class", "line")
+
+    yDots
+        .append("line")
+        .attr("class", "dot")
+        .attr("y1", 0)
+        .attr("y2", 0)
+        .attr("x1", 0)
+        .attr("x2", width - padding.right + padding.left/2)
+    yDots
+        .append("text")
+        .attr("x", width - padding.right + padding.left)
+        .attr("y", 6)
+        .attr("text-anchor", "start")
+        .text(function (d, i) { return i == 0 ? "pages" : d; })
+
+    /* Bars */
+    var shelf = bars.selectAll('g.shelf').data(window.books, function (d) { return d.title; })
+
+    shelf.exit()
+        .remove()
+
+    // add group for each book, positioning the bar on `x`
+    shelf.enter().append("g")
+        .attr("transform", function (d) { return "translate(" + x(d.start_date) + ", 0)"; })
+        .attr("class", function (d) { return "shelf month-" + (d.start_date.getMonth() + 1); })
+
+    // the actual element is a rect and is computing width and height
+    var rects = shelf.append("rect")
+        .attr("class", "book")
+        // need to define values before the transition
+        .attr("y", function (d) { return y(0); })
+        .attr("height", function (d) { return height - y(0); })
+
+    // and then give them the real value
+    rects.transition()
+        .duration(300)
+        .attr("width", function (d) { return x(d.end_date) - x(d.start_date); })
+        .attr("y", function (d) { return y(d.pages); })
+        .attr("height", function (d) { return height - y(d.pages); })
+
+    // Interactions
+    rects.on('mouseover', function (d) {
+        var book = d3.select(this);
+        book.attr("class", "book active");
+        d3.select("#placeholder").style("display", "none");
+        d3.select("#book-detail").html("" +
+            '<h3 class="title">' + d.title + '</h3>' +
+            '<h4 class="info">' +
+            "written by<em> " + d.author + " </em>in" +
+            "<em> " + d.year + " </em>.</h4>");
+
+        pages.mouseover(d);
+    })
+    .on("click", function () {
+        // disable the mouseleave and re-enable on the click on the body
+        clicked = true;
+        d3.event.stopPropagation()
+    })
+
+    pages = new Pages();
+
+    data = {
         books: books.length,
         pages: d3.sum(books, function (d) { return d.pages; }),
         days: d3.sum(books, function (d) { return (d.end_date - d.start_date) / (1000*60*60*24); })
     };
+
     data.average = data.pages / data.days;
 
     d3.selectAll(".d3-data").each(showData(data));
 
-    Pages = function () {
-        this.node = svg.append("g")
-            .attr("class", "pages")
-            .attr("transform", "translate(0, " + (y(0) + padding.top) + ")")
+}
 
-        this.line = this.node.append("line")
-            .attr("class", "dot")
-            .attr("y1", 0)
-            .attr("y2", 0)
-            .attr("x1", 0)
-            .attr("x2", width - padding.right + padding.left/2)
-        this.text = this.node.append("text")
-            .attr("x", width - padding.right + padding.left)
-            .attr("y", 6)
-            .attr("text-anchor", "start")
+var data, pages;
 
-        this.range = svg.append("g")
-            .attr("class", "range")
-            .attr("transform", "translate(" + (padding.left) + ", 0)")
+function load(year) {
+    d3.json("./data/books-" + year + ".json", function (books) {
+        window.books = books;
+        books = preformat(books);
+        update(year, books);
+    });
+}
 
-        this.start_date = this.range.append("line")
-            .attr("class", "start_date")
-            .attr("x1", 0)
-            .attr("x2", 0)
-            .attr("y1", 0)
-            .attr("y2", height + padding.top + padding.bottom)
-
-        this.end_date = this.range.append("line")
-            .attr("class", "end_date")
-            .attr("x1", 0)
-            .attr("x2", 0)
-            .attr("y1", 0)
-            .attr("y2", height + padding.top + padding.bottom)
-
-        this.start_dateText = this.range.append("text")
-            .style("text-anchor", "end")
-            .attr("y", height + padding.top + padding.bottom - 6)
-
-        this.end_dateText = this.range.append("text")
-            .attr("class", "")
-            .style("text-anchor", "start")
-            .attr("y", height + padding.top + padding.bottom - 6)
-
-        this.node.style("display", "none");
-        this.range.style("display", "none");
-    };
-    Pages.prototype.mouseover = function (d) {
-        d3.selectAll(".lines.y g:not(:first-child), .lines.x").style("display", "none");
-        this.node.style("display", "block");
-        this.range.style("display", "block");
-
-        this.node
-            .transition()
-            .duration(750)
-            .ease("cubic-out")
-            .attr("transform", "translate(0, " + (y(d.pages) + padding.top) + ")")
-        this.text.text(d.pages)
-
-        this.start_date.attr("transform", "translate(" + x(d.start_date) + ", 0)")
-        this.end_date.attr("transform", "translate(" + x(d.end_date) + ", 0)")
-        this.start_dateText
-            .attr("x", x(d.start_date) - 5)
-            .text(d3.time.format("%b. %d")(d.start_date))
-
-        this.end_dateText
-            .attr("x", x(d.end_date) + 5)
-            .text(d3.time.format("%b. %d")(d.end_date))
-
-        d3.selectAll(".d3-data").each(showData(d));
-    };
-    Pages.prototype.mouseout = function () {
-        d3.selectAll(".lines.y g:not(:first-child), .lines.x").style("display", "block")
-        this.node.style("display", "none")
-        this.range.style("display", "none")
-
-         d3.selectAll(".d3-data").each(showData(data));
-    };
-
-    var pages = new Pages();
-
-    var clicked = false;
-
-    d3.selectAll(".book")
-        .on("mouseover", function (d) {
-            var book = d3.select(this);
-            book.attr("class", "book active");
-            d3.select("#placeholder").style("display", "none");
-            d3.select("#book-detail").html("" +
-                '<h3 class="title">' + d.title + '</h3>' +
-                '<h4 class="info">' +
-                "written by<em> " + d.author + " </em>in" +
-                "<em> " + d.year + " </em>.</h4>");
-
-            pages.mouseover(d);
-        })
-        .on("click", function () {
-            // disable the mouseleave and re-enable on the click on the body
-            clicked = true;
-            d3.event.stopPropagation()
-        });
-
-    function mouseout () {
-        d3.select("#placeholder").style("display", "block");
-        d3.select("#book-detail").html("")
-        pages.mouseout();
-    }
-
-    d3.select("#book-wrapper")
-        .on("mouseleave", function () {
-            if (!clicked) {
-                mouseout();
-            }
-        })
-
-    d3.select("body")
-        .on("click", function () {
-            mouseout();
-            clicked = false;
-        });
-});
 
 var margin = {
         top: 20,
@@ -273,3 +185,153 @@ svg.append('rect')
     .attr('y', 0)
     .attr('width', width + margin.left + margin.right + padding.left + padding.right)
     .attr('height', height + margin.top + margin.bottom + padding.top + padding.bottom);
+
+var x = d3.time.scale()
+        .range([0, width - padding.left - padding.right]);
+var y = d3.scale.linear()
+        .rangeRound([height, 0]);
+
+var yLines = svg.append("g")
+    .attr("transform", "translate(0, " + padding.top + ")")
+    .attr("class", "lines y")
+
+var xLines = svg.append("g")
+    .attr("transform", "translate(" + padding.left + ", " + (height + padding.top) + ")")
+    .attr("class", "lines x")
+
+window.books = [];
+var bars = svg.append("g")
+    .attr("transform", "translate(" + padding.left + ", " + (padding.top + 1) + ")")
+    .attr("class", "bars")
+
+
+
+/* HTML representation */
+
+function showData(d) {
+    return function () {
+        var node = d3.select(this),
+            attribute = node.attr('data-attribute'),
+            pluralize = node.attr('data-pluralize'),
+            format = node.attr("data-format") || ".1f";
+
+        if  (pluralize !== null) {
+            if (d[attribute] != 1) {
+                pluralize += 's';
+            }
+            node.text(pluralize);
+        } else {
+            node.text(d3.format(format)(d[attribute]));
+        }
+    }
+}
+
+Pages = function () {
+    this.node = svg.append("g")
+        .attr("class", "pages")
+        .attr("transform", "translate(0, " + (y(0) + padding.top) + ")")
+
+    this.line = this.node.append("line")
+        .attr("class", "dot")
+        .attr("y1", 0)
+        .attr("y2", 0)
+        .attr("x1", 0)
+        .attr("x2", width - padding.right + padding.left/2)
+    this.text = this.node.append("text")
+        .attr("x", width - padding.right + padding.left)
+        .attr("y", 6)
+        .attr("text-anchor", "start")
+
+    this.range = svg.append("g")
+        .attr("class", "range")
+        .attr("transform", "translate(" + (padding.left) + ", 0)")
+
+    this.start_date = this.range.append("line")
+        .attr("class", "start_date")
+        .attr("x1", 0)
+        .attr("x2", 0)
+        .attr("y1", 0)
+        .attr("y2", height + padding.top + padding.bottom)
+
+    this.end_date = this.range.append("line")
+        .attr("class", "end_date")
+        .attr("x1", 0)
+        .attr("x2", 0)
+        .attr("y1", 0)
+        .attr("y2", height + padding.top + padding.bottom)
+
+    this.start_dateText = this.range.append("text")
+        .style("text-anchor", "end")
+        .attr("y", height + padding.top + padding.bottom - 6)
+
+    this.end_dateText = this.range.append("text")
+        .attr("class", "")
+        .style("text-anchor", "start")
+        .attr("y", height + padding.top + padding.bottom - 6)
+
+    this.node.style("display", "none");
+    this.range.style("display", "none");
+};
+
+Pages.prototype.mouseover = function (d) {
+    d3.selectAll(".lines.y g:not(:first-child), .lines.x").style("display", "none");
+    this.node.style("display", "block");
+    this.range.style("display", "block");
+
+    this.node
+        .transition()
+        .duration(750)
+        .ease("cubic-out")
+        .attr("transform", function () {
+            var limit = 0.75,
+                max = y(0);
+            // define min value so it's not overlapping with "pages" label
+            var yValue = (max * limit < y(d.pages)) ? Math.floor(max * limit) : y(d.pages);
+            return "translate(0, " + (yValue + padding.top) + ")";
+        });
+    this.text.text(d.pages)
+
+    this.start_date.attr("transform", "translate(" + x(d.start_date) + ", 0)")
+    this.end_date.attr("transform", "translate(" + x(d.end_date) + ", 0)")
+    this.start_dateText
+        .attr("x", x(d.start_date) - 5)
+        .text(d3.time.format("%b. %d")(d.start_date))
+
+    this.end_dateText
+        .attr("x", x(d.end_date) + 5)
+        .text(d3.time.format("%b. %d")(d.end_date))
+
+    d3.selectAll(".d3-data").each(showData(d));
+};
+Pages.prototype.mouseout = function () {
+    d3.selectAll(".lines.y g:not(:first-child), .lines.x").style("display", "block")
+    this.node.style("display", "none")
+    this.range.style("display", "none")
+
+     d3.selectAll(".d3-data").each(showData(data));
+};
+
+
+// cancel/reset click behavior
+var clicked = false;
+function mouseout () {
+    d3.select("#placeholder").style("display", "block");
+    d3.select("#book-detail").html("")
+    pages.mouseout();
+}
+
+d3.select("#book-wrapper")
+    .on("mouseleave", function () {
+        if (!clicked) {
+            mouseout();
+        }
+    })
+
+d3.select("body")
+    .on("click", function () {
+        mouseout();
+        clicked = false;
+    });
+
+// main
+load(2014)
